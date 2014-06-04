d4179fc4a71bb28401be4598d74f854a6f869662
Tara Lerias <tlerias@gmail.com>
2014-06-04 12:18:04 -0400

    gruntfile minificaation for CSS & JS

---
+++ Gruntfile.js
@@ -0,0 +1,91 @@
+module.exports = function(grunt){
+
+  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    htmlhint: {
+      build: {
+        options: {
+          'tag-pair': true,
+          'tagname-lowercase': true,
+          'attr-lowercase': true,
+          'attr-value-double-quotes': true,
+          'doctype-first': true,
+          'spec-char-escape': true,
+          'id-unique': true,
+          'head-script-disabled': true,
+          'style-disabled': true
+        },
+        src: ['index.html']
+      }
+    },
+
+    watch: {
+      html: {
+        files: ['**/.html'],
+        tasks: ['htmlhint']
+      },
+      js:{
+        files: ['dest/scripts/script.js'],
+        tasks: ['uglify']
+      },
+      css: {
+        files: [
+          'assets/**/*.scss',
+          'public/stylesheets/**/*.css'
+        ],
+        tasks: ['buildcss']
+      }
+    },
+
+    uglify: {
+      build: {
+        files: {
+          'dest/scripts/script.js': [
+            'public/**/*.js',
+            'models/**/*.js',
+            'routes/**/*.js',
+            'app/**/*.js'
+            ]
+        }
+      }
+    },
+
+    cssc: {
+      build: {
+        options: {
+          consolidateViaDeclarations: true,
+          consolidateViaSelectors: true,
+          consolidateMediaQueries: true
+        },
+        files: {
+          'dest/stylesheets/style.css': [
+            'assets/**/*.scss',
+            'public/stylesheets/**/*.css'
+          ]
+        }
+      }
+    },
+
+    cssmin: {
+      build: {
+        src: 'dest/stylesheets/style.css',
+        dest: 'dest/stylesheets/style.css'
+      }
+    },
+
+    sass: {
+      build: {
+        files: {
+          'dest/stylesheets/style.css': 'assets/stylesheets/*.scss'
+        }
+      }
+    }
+  });
+
+  grunt.registerTask('default', []);
+  grunt.registerTask('htmlhint', []);
+  grunt.registerTask('build',  ['sass', 'cssc', 'cssmin', 'uglify']);
+};
